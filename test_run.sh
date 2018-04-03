1212e782d8f6ffbb817f74f3a5052ffe4a3d2f9a
inocop <inoue19851110@gmail.com>
2018-04-03 14:33:27 +0900

    Update test_run.sh

--- test_run.sh
+++ test_run.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-# MSYS_NO_PATHCONV=1 for git bash
+# MSYS_NO_PATHCONV=1 for windows git bash
 MSYS_NO_PATHCONV=1 docker run -it --rm --volume `pwd`:/var/nagoyaphp12 ruby:2.4.4-alpine ruby /var/nagoyaphp12/src/test.rb
